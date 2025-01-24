9b012dc1a73cc0b406db23c7f7b10e4c33cd86b9
gfdgd_xi <3025613752@qq.com>
2025-01-24 10:23:58 +0800

    补充依赖

--- build-deb.sh
+++ build-deb.sh
@@ -14,7 +14,7 @@ sudo apt build-dep -y linux
 sudo apt install gcc-riscv64-linux-gnu gcc-mips-linux-gnu gcc-aarch64-linux-gnu gcc-mips64el-linux-gnuabi64 -y
 sudo apt install gcc-loongarch64-linux-gnu -y
 sudo apt install -y libssl-dev wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git vim libelf-dev zstd
-sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev
+sudo apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev git lsb vim libelf-dev libwolfssl-dev
 # 解压源码包
 aria2c $kernelUrl
 tar -xf $(basename $kernelUrl)
